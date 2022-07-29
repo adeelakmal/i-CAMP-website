ede91b01afaae85dc2c9c45f7b761fbe18820342
adeelakmal <adeelakmal0@gmail.com>
2022-07-29 13:15:36 +0500

    added contct form and styled scrollbar
    scroll and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,140 @@
-import React from 'react'
+import React from "react";
 
 export default function Contact() {
   return (
     <>
-    orignal copying inst worky so idk
+      <section className="bg-light py-5">
+        <div className="container px-5 my-5 px-5">
+          <div className="text-center mb-5">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
+              <i className="bi bi-envelope"></i>
+            </div>
+            <h2 className="fw-bolder">Get in touch</h2>
+            <p className="lead mb-0">We'd love to hear from you</p>
+          </div>
+          <div className="row gx-5 justify-content-center">
+            <div className="col-lg-6">
+              {/* <!-- * * * * * * * * * * * * * * *--> */}
+              {/* <!-- * * SB Forms Contact Form * *--> */}
+              {/* <!-- * * * * * * * * * * * * * * *--> */}
+              {/* <!-- This form is pre-integrated with SB Forms.--> */}
+              {/* <!-- To make this form functional, sign up at--> */}
+              {/* <!-- https://startbootstrap.com/solution/contact-forms--> */}
+              {/* <!-- to get an API token!--> */}
+              <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                {/* <!-- Name input--> */}
+                <div className="form-floating mb-3">
+                  <input
+                    className="form-control"
+                    id="name"
+                    type="text"
+                    placeholder="Enter your name..."
+                    data-sb-validations="required"
+                  />
+                  <label for="name">Full name</label>
+                  <div
+                    className="invalid-feedback"
+                    data-sb-feedback="name:required"
+                  >
+                    A name is required.
+                  </div>
+                </div>
+                {/* <!-- Email address input--> */}
+                <div className="form-floating mb-3">
+                  <input
+                    className="form-control"
+                    id="email"
+                    type="email"
+                    placeholder="name@example.com"
+                    data-sb-validations="required,email"
+                  />
+                  <label for="email">Email address</label>
+                  <div
+                    className="invalid-feedback"
+                    data-sb-feedback="email:required"
+                  >
+                    An email is required.
+                  </div>
+                  <div
+                    className="invalid-feedback"
+                    data-sb-feedback="email:email"
+                  >
+                    Email is not valid.
+                  </div>
+                </div>
+                {/* <!-- Phone number input--> */}
+                <div className="form-floating mb-3">
+                  <input
+                    className="form-control"
+                    id="phone"
+                    type="tel"
+                    placeholder="(123) 456-7890"
+                    data-sb-validations="required"
+                  />
+                  <label for="phone">Phone number</label>
+                  <div
+                    className="invalid-feedback"
+                    data-sb-feedback="phone:required"
+                  >
+                    A phone number is required.
+                  </div>
+                </div>
+                {/* <!-- Message input--> */}
+                <div className="form-floating mb-3">
+                  <textarea
+                    className="form-control"
+                    id="message"
+                    type="text"
+                    placeholder="Enter your message here..."
+                    style={{ height: "10rem" }}
+                    data-sb-validations="required"
+                  ></textarea>
+                  <label for="message">Message</label>
+                  <div
+                    className="invalid-feedback"
+                    data-sb-feedback="message:required"
+                  >
+                    A message is required.
+                  </div>
+                </div>
+                {/* <!-- Submit success message--> */}
+                {/* <!----> */}
+                {/* <!-- This is what your users will see when the form--> */}
+                {/* <!-- has successfully submitted--> */}
+                <div className="d-none" id="submitSuccessMessage">
+                  <div className="text-center mb-3">
+                    <div className="fw-bolder">Form submission successful!</div>
+                    To activate this form, sign up at
+                    <br />
+                    <a href="https://startbootstrap.com/solution/contact-forms">
+                      https://startbootstrap.com/solution/contact-forms
+                    </a>
+                  </div>
+                </div>
+                {/* <!-- Submit error message--> */}
+                {/* <!----> */}
+                {/* <!-- This is what your users will see when there is--> */}
+                {/* <!-- an error submitting the form--> */}
+                <div className="d-none" id="submitErrorMessage">
+                  <div className="text-center text-danger mb-3">
+                    Error sending message!
+                  </div>
+                </div>
+                {/* <!-- Submit Button--> */}
+                <div className="d-grid">
+                  <button
+                    className="btn btn-primary btn-lg"
+                    id="submitButton"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
