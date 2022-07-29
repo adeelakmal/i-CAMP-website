7f24a6e5f4468afef5f1f7cd6892c90935b068b2
arhm098 <myhammad.arham.0987711@gmail.com>
2022-07-29 01:10:00 -0700

    features tab done
    still working the color under the graph so dont touch that

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav className ="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className ="container px-5">
-                <a className ="navbar-brand" href="#!">Start Bootstrap</a>
+                <a className ="navbar-brand" href="#!">I-CAMP</a>
                 <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className ="navbar-toggler-icon"></span></button>
                 <div className ="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className ="navbar-nav ms-auto mb-2 mb-lg-0">
