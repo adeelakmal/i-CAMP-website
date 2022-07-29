c55831ec389b67b6060b5429783ce91e5d224f6f
adeelakmal <adeelakmal0@gmail.com>
2022-07-29 13:57:51 +0500

    made features responsive
    container-md go brrrrr

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Features() {
-
   const style = `
   li::before {
   
@@ -11,58 +10,59 @@ function Features() {
     width: 1em; 
     margin-left: -1em; 
   }`;
-  
+
   return (
     <>
       <style>{style}</style>
       {/*Features section */}
       <div className="lead text-center mt-5">
-            <h2 className="fw-bold">OUR SERVICES</h2>
-          </div>
-      <div className="Container">
-        <div className="row">
-          <div className="col p-5">
-            <p  className="lead text-black-50 mb-4 fw-normal">
-            i-CAMP is a conglomeration of award-winning talent from a variety of 
-            domains, owing to the multi-faceted nature of institutions.
+        <h2 className="fw-bold">OUR SERVICES</h2>
+      </div>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-9 col-md-6 p-5">
+            <p className="lead text-black-50 mb-4 fw-normal">
+              i-CAMP is a conglomeration of award-winning talent from a variety
+              of domains, owing to the multi-faceted nature of institutions.
             </p>
             <p className="lead text-black-50 mb-4 fw-normal">
-            We have nurtured two domains extensively,
+              We have nurtured two domains extensively,
             </p>
             <p className="lead text-black-50 mb-4 fw-normal">
-              <span className="fw-bolder">Business</span> Development & <span className="fw-bolder">Software</span> Development 
-          </p>
-          <p className="lead text-black-50 mb-4 fw-normal">
-          We have <span className="fw-bolder">generated</span> revenue in the order of <span className="fw-bolder">millions</span> and have 
-          ensured customer satisfaction, our KPI being our <span className="fw-bolder">low churn</span> rate
-          </p>
+              <span className="fw-bolder">Business</span> Development &{" "}
+              <span className="fw-bolder">Software</span> Development
+            </p>
+            <p className="lead text-black-50 mb-4 fw-normal">
+              We have <span className="fw-bolder">generated</span> revenue in
+              the order of <span className="fw-bolder">millions</span> and have
+              ensured customer satisfaction, our KPI being our{" "}
+              <span className="fw-bolder">low churn</span> rate
+            </p>
           </div>
-        <div className="col">
-          <figure>
-            <svg viewBox="0 0 63.6619772368 63.6619772368">
-              <circle
-                className ="pie1"
-                cx="31.8309886184"
-                cy="31.8309886184"
-                r="15.9154943092"
+          <div className="col-9 col-md-6  py-5">
+            <figure>
+              <svg viewBox="0 0 63.6619772368 63.6619772368">
+                <circle
+                  className="pie1"
+                  cx="31.8309886184"
+                  cy="31.8309886184"
+                  r="15.9154943092"
                 />
-              <circle
-                className ="pie2"
-                cx="31.8309886184"
-                cy="31.8309886184"
-                r="15.9154943092"
+                <circle
+                  className="pie2"
+                  cx="31.8309886184"
+                  cy="31.8309886184"
+                  r="15.9154943092"
                 />
-            </svg>
-            
-          </figure>
-          <div className="lead text-center d-flex justify-content-center mt-2">
-            <li className="mx-3">10 in Tech </li>
-            <li>17 in Business</li>
+              </svg>
+            </figure>
+            <div className="lead text-center d-flex justify-content-center mt-2">
+              <li className="mx-3">10 in Tech </li>
+              <li>17 in Business</li>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
     </>
   );
 }
