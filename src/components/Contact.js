1e578fda2ca07ba026e6d87c5d1079ad6fa689b5
ShaheerRehman <shaheerr7@gmail.com>
2022-08-01 01:08:11 +0500

    tried fetching

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [contactData, setContactData]=useState({
+    'name': '',
+    'email': '',
+    'phone': '',
+    'describe': '',
+    'status': '',
+  });
+  
+  const change=(event)=>{
+    setContactData({
+      ...contactData,
+      [event.target.name]: event.target.value
+    });
+  }
+  const submitForm=()=>{
+    const contactFormData=new FormData();
+    contactFormData.append('name', contactData.name)
+    contactFormData.append('email', contactData.email)
+    contactFormData.append('phone', contactData.phone)
+    contactFormData.append('describe', contactData.describe)
+    fetch('http://127.0.0.1:8000/ask/', {
+      mode: 'cors',
+  method: 'POST',
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    'Content-Type': 'application/json',
+  },
+   body: JSON.stringify({
+     // your expected POST request payload goes here
+        name: contactData.name,
+        email: contactData.email,
+        phone:contactData.phone,
+        describe: contactData.describe
+      })
+})
+  .then(res => res.json())
+  .then(data => {
+      // enter you logic when the fetch is successful
+      setContactData({
+        'name': '',
+        'email': '',
+        'phone': '',
+        'describe': '',
+        'status': 'success',
+      })
+    }
+  )
+  .catch(error => {
+  // enter your logic for when there is an error (ex. error toast)
+   console.log(error)
+  })
+  
+  //
+  // const axiosPostCall = async () => {
+  //   try {
+  //     const { data } = await axios.post('http://127.0.0.1:8000/ask/', {
+  //       name: contactData.name,
+  //       email: contactData.email,
+  //       phone:contactData.phone,
+  //       describe: contactData.describe
+  //     }).then((response)=>{setContactData({
+  //         'name':'',
+  //         'email': '',
+  //         'phone': '',
+  //         'describe': '',
+  //         'status': 'success',
+  //       });
+  //     })
+  //  // enter you logic when the fetch is successful
+  //     console.log("success")
+  //
+  //   } catch (error) {
+  // // enter your logic for when there is an error (ex. error toast)
+  //     console.log(`error: `, error)
+  //     setContactData({status: 'error'})
+  //   }
+  // }
+  // axiosPostCall()
+
+    // try{
+    //   axios.post('http://127.0.0.1:8000/ask/', contactFormData)
+    //     .then((response)=>{
+    //     setContactData({
+    //       'name':'',
+    //       'email': '',
+    //       'phone': '',
+    //       'describe': '',
+    //       'status': 'success',
+    //     });
+    //   })
+    // }catch (error){
+    //   console.log(error);
+    //   // setContactData({'status': 'error'})
+    // }
+  }
   return (
     <>
       <section className="bg-light py-5">
@@ -14,24 +109,21 @@ export default function Contact() {
           </div>
           <div className="row gx-5 justify-content-center">
             <div className="col-lg-6">
-              {/* <!-- * * * * * * * * * * * * * * *--> */}
-              {/* <!-- * * SB Forms Contact Form * *--> */}
-              {/* <!-- * * * * * * * * * * * * * * *--> */}
-              {/* <!-- This form is pre-integrated with SB Forms.--> */}
-              {/* <!-- To make this form functional, sign up at--> */}
-              {/* <!-- https://startbootstrap.com/solution/contact-forms--> */}
-              {/* <!-- to get an API token!--> */}
               <form id="contactForm" data-sb-form-api-token="API_TOKEN">
                 {/* <!-- Name input--> */}
                 <div className="form-floating mb-3">
+                  {contactData.status==='success'&&<p className='text-success'>Your query has been sent</p>}
+                  {contactData.status==='error'&&<p className='text-danger'>Something went wrong!</p>}
                   <input
                     className="form-control"
                     id="name"
                     type="text"
                     placeholder="Enter your name..."
+                    defaultValue={contactData.name}
+                    onChange={change}
                     data-sb-validations="required"
                   />
-                  <label for="name">Full name</label>
+                  <label htmlFor="name">Full name</label>
                   <div
                     className="invalid-feedback"
                     data-sb-feedback="name:required"
@@ -46,9 +138,11 @@ export default function Contact() {
                     id="email"
                     type="email"
                     placeholder="name@example.com"
+                    defaultValue={contactData.email}
+                    onChange={change}
                     data-sb-validations="required,email"
                   />
-                  <label for="email">Email address</label>
+                  <label htmlFor="email">Email address</label>
                   <div
                     className="invalid-feedback"
                     data-sb-feedback="email:required"
@@ -68,10 +162,12 @@ export default function Contact() {
                     className="form-control"
                     id="phone"
                     type="tel"
+                    defaultValue={contactData.phone}
+                    onChange={change}
                     placeholder="(123) 456-7890"
                     data-sb-validations="required"
                   />
-                  <label for="phone">Phone number</label>
+                  <label htmlFor="phone">Phone number</label>
                   <div
                     className="invalid-feedback"
                     data-sb-feedback="phone:required"
@@ -86,10 +182,12 @@ export default function Contact() {
                     id="message"
                     type="text"
                     placeholder="Enter your message here..."
+                    defaultValue={contactData.describe}
+                    onChange={change}
                     style={{ height: "10rem" }}
                     data-sb-validations="required"
                   ></textarea>
-                  <label for="message">Message</label>
+                  <label htmlFor="message">Message</label>
                   <div
                     className="invalid-feedback"
                     data-sb-feedback="message:required"
@@ -97,18 +195,14 @@ export default function Contact() {
                     A message is required.
                   </div>
                 </div>
-                {/* <!-- Submit success message--> */}
-                {/* <!----> */}
-                {/* <!-- This is what your users will see when the form--> */}
-                {/* <!-- has successfully submitted--> */}
                 <div className="d-none" id="submitSuccessMessage">
                   <div className="text-center mb-3">
                     <div className="fw-bolder">Form submission successful!</div>
-                    To activate this form, sign up at
-                    <br />
-                    <a href="https://startbootstrap.com/solution/contact-forms">
-                      https://startbootstrap.com/solution/contact-forms
-                    </a>
+                    {/*To activate this form, sign up at*/}
+                    {/*<br />*/}
+                    {/*<a href="https://startbootstrap.com/solution/contact-forms">*/}
+                    {/*  https://startbootstrap.com/solution/contact-forms*/}
+                    {/*</a>*/}
                   </div>
                 </div>
                 {/* <!-- Submit error message--> */}
@@ -126,6 +220,7 @@ export default function Contact() {
                     className="btn btn-primary btn-lg"
                     id="submitButton"
                     type="submit"
+                    onClick={submitForm}
                   >
                     Submit
                   </button>
