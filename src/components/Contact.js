b17b9fbb431fe2aeaac3ab4bdbf8ec6ed085a94d
ShaheerRehman <shaheerr7@gmail.com>
2022-08-01 22:03:36 +0500

    contact component finished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,39 +1,16 @@
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
 
 export default function Contact() {
   const [sendRequest, setSendRequest] = useState(false);
-  const [contactData, setContactData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    describe: "",
-    status: "",
-  });
+  
   const [Name, setName] = useState("");
   const [Email, setEmail] = useState("");
   const [Phone, setPhone] = useState("");
   const [Message, setMessage] = useState("");
-
-  // const change = (event) => {
-  //   setContactData({
-  //     ...contactData,
-  //     [event.target.name]: event.target.value,
-
-  //   });
-  // }
-  // var doThis = (event) => {
-  //   event.preventDefault();
-  //   console.log(`This is the name ${Name}`);
-  // };
-
-  var contactFormData = new FormData();
+  const [Status, setStatus] = useState("");
+  
   const SubmitForm = (event) => {
     event.preventDefault();
-    contactFormData.append("name", Name);
-    contactFormData.append("email", Email);
-    contactFormData.append("phone", Phone);
-    contactFormData.append("describe", Message);
     setSendRequest(true);
   };
   useEffect(() => {
@@ -41,24 +18,22 @@ export default function Contact() {
       fetch(`http://127.0.0.1:8000/ask/`, {
         method: "POST",
         headers: {
-          // "Access-Control-Allow-Origin": "*",
           "Content-Type": "application/json",
-          Authorization: "Token 757ab0fafa75f4c43646eedc5c8a83fc6b358592",
         },
         body: JSON.stringify({
           // your expected POST request payload goes here
-          name: contactFormData.name,
-          email: "shaheerr7@gmail.com",
-          phone: "3127950074",
-          describe: "Help me",
+          name: Name,
+          email: Email,
+          phone: Phone,
+          describe: Message,
         }),
       })
         .then((resp) => resp.json())
-        .then((resp) => console.log(resp))
-        .catch((error) => console.log(error));
+        .then(() => setStatus('success'))
+        .catch(() => setStatus('error'));
     }
     setSendRequest(false);
-  }, [sendRequest, contactFormData]);
+  }, [sendRequest]);
 
   return (
     <>
@@ -69,25 +44,28 @@ export default function Contact() {
               <i className="bi bi-envelope"></i>
             </div>
             <h2 className="fw-bolder">Get in touch</h2>
-            <p className="lead mb-0">We'd love to hear from you</p>
+            {Status === '' && (
+              <p className="lead mb-0">We'd love to hear from you</p>
+              )}
+            {Status === "success" && (
+              <p className="text-success">Your query has been sent</p>
+              )}
+            {Status === "error" && (
+              <p className="text-danger">Something went wrong!</p>
+              )}
           </div>
           <div className="row gx-5 justify-content-center">
             <div className="col-lg-6">
               <form id="contactForm" onSubmit={SubmitForm}>
                 {/* <!-- Name input--> */}
                 <div className="form-floating mb-3">
-                  {contactData.status === "success" && (
-                    <p className="text-success">Your query has been sent</p>
-                  )}
-                  {contactData.status === "error" && (
-                    <p className="text-danger">Something went wrong!</p>
-                  )}
+                  
                   <input
                     className="form-control"
                     id="name"
                     type="text"
                     placeholder="Enter your name..."
-                    defaultValue={contactData.name}
+                    defaultValue={Name}
                     onChange={(event) => setName(event.target.value)}
                     data-sb-validations="required"
                   />
@@ -106,7 +84,7 @@ export default function Contact() {
                     id="email"
                     type="email"
                     placeholder="name@example.com"
-                    defaultValue={contactData.email}
+                    defaultValue={Email}
                     onChange={(event) => setEmail(event.target.value)}
                     data-sb-validations="required,email"
                   />
@@ -130,7 +108,7 @@ export default function Contact() {
                     className="form-control"
                     id="phone"
                     type="tel"
-                    defaultValue={contactData.phone}
+                    defaultValue={Phone}
                     onChange={(event) => setPhone(event.target.value)}
                     placeholder="(123) 456-7890"
                     data-sb-validations="required"
@@ -150,7 +128,7 @@ export default function Contact() {
                     id="message"
                     datatype="text"
                     placeholder="Enter your message here..."
-                    defaultValue={contactData.describe}
+                    defaultValue={Message}
                     onChange={(event) => setMessage(event.target.value)}
                     style={{ height: "10rem" }}
                     data-sb-validations="required"
@@ -166,17 +144,10 @@ export default function Contact() {
                 <div className="d-none" id="submitSuccessMessage">
                   <div className="text-center mb-3">
                     <div className="fw-bolder">Form submission successful!</div>
-                    {/*To activate this form, sign up at*/}
-                    {/*<br />*/}
-                    {/*<a href="https://startbootstrap.com/solution/contact-forms">*/}
-                    {/*  https://startbootstrap.com/solution/contact-forms*/}
-                    {/*</a>*/}
+                    
                   </div>
                 </div>
-                {/* <!-- Submit error message--> */}
-                {/* <!----> */}
-                {/* <!-- This is what your users will see when there is--> */}
-                {/* <!-- an error submitting the form--> */}
+                
                 <div className="d-none" id="submitErrorMessage">
                   <div className="text-center text-danger mb-3">
                     Error sending message!
@@ -184,15 +155,37 @@ export default function Contact() {
                 </div>
                 {/*// <!-- Submit Button-->*/}
                 <div className="d-grid">
+                  {Status === '' && (
                   <button
                     className="btn btn-primary btn-lg"
                     id="submitButton"
                     type="submit"
                     disabled={sendRequest}
-                    // onClick={doThis}
                   >
                     Submit
                   </button>
+                  )}
+                {Status === "success" && (
+                  <button
+                    className="btn btn-success btn-lg"
+                    id="submitButton"
+                    type="submit"
+                    disabled={sendRequest}
+                  >
+                    Submit
+                  </button>
+                  )}
+                {Status === "error" && (
+                  <button
+                    className="btn btn-danger btn-lg"
+                    id="submitButton"
+                    type="submit"
+                    disabled={sendRequest}
+                  >
+                    Submit
+                  </button>
+                  )}
+                  
                 </div>
               </form>
             </div>
