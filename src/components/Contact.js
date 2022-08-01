b6ee76a31398f7641ae21926c507592693fb3379
ShaheerRehman <shaheerr7@gmail.com>
2022-08-01 10:02:57 +0500

    tried fetching

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { API } from "./api-service";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Contact() {
   const [contactData, setContactData]=useState({
@@ -13,116 +13,35 @@ export default function Contact() {
   const change=(event)=>{
     setContactData({
       ...contactData,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+    
     });
   }
-//
-//   useEffect(
-//     fetch(`http://127.0.0.1:8000/api/movies/`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         // "Authorization": "Token 757ab0fafa75f4c43646eedc5c8a83fc6b358592"
-//       },
-//    body: JSON.stringify({
-//      // your expected POST request payload goes here
-//         title: "Dont look up",
-//         description: "comedy"
-//       })
-// })
-//   .then(resp => resp.json())
-//   .then(resp => console.log(resp)
-//     // {
-//     //   // enter you logic when the fetch is successful
-//     //   setContactData({
-//     //     'name': '',
-//     //     'email': '',
-//     //     'phone': '',
-//     //     'describe': '',
-//     //     'status': 'success',
-//     //   }
-//       ).catch(error=>console.log(error))
-//     ,[])
-//
-  const submitForm=()=> {
-    // const contactFormData=new FormData();
-    // contactFormData.append('name', contactData.name)
-    // contactFormData.append('email', contactData.email)
-    // contactFormData.append('phone', contactData.phone)
-    // contactFormData.append('describe', contactData.describe)
-    API.create().then(resp=>console.log(resp)).catch(error=>console.log(error))
-  }
-//   fetch(`http://127.0.0.1:8000/ask/`, {
-//       method: 'POST',
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         'Content-Type': 'application/json',
-//         "Authorization": "Token 757ab0fafa75f4c43646eedc5c8a83fc6b358592"
-//       },
-//    body: JSON.stringify({
-//      // your expected POST request payload goes here
-//         name: "Shaheer",
-//         email: "shaheerr7@gmail.com",
-//         phone:"3127950074",
-//         describe: "Help me"
-//       })
-// })
-//   .then(resp => resp.json())
-//   .then(resp => console.log(resp)
-//     // {
-//     //   // enter you logic when the fetch is successful
-//     //   setContactData({
-//     //     'name': '',
-//     //     'email': '',
-//     //     'phone': '',
-//     //     'describe': '',
-//     //     'status': 'success',
-//     //   }
-//       ).catch(error=>console.log(error))
-//     }
-//
-  //
-  // const axiosPostCall = async () => {
-  //   try {
-  //     const { data } = await axios.post('http://127.0.0.1:8000/ask/', {
-  //       name: contactData.name,
-  //       email: contactData.email,
-  //       phone:contactData.phone,
-  //       describe: contactData.describe
-  //     }).then((response)=>{setContactData({
-  //         'name':'',
-  //         'email': '',
-  //         'phone': '',
-  //         'describe': '',
-  //         'status': 'success',
-  //       });
-  //     })
-  //  // enter you logic when the fetch is successful
-  //     console.log("success")
-  //
-  //   } catch (error) {
-  // // enter your logic for when there is an error (ex. error toast)
-  //     console.log(`error: `, error)
-  //     setContactData({status: 'error'})
-  //   }
-  // }
-  // axiosPostCall()
+  
+  
+  const submitForm = () => {
+    const contactFormData = new FormData();
+    contactFormData.append('name', contactData.name)
+    contactFormData.append('email', contactData.email)
+    contactFormData.append('phone', contactData.phone)
+    contactFormData.append('describe', contactData.describe)
 
-    // try{
-    //   axios.post('http://127.0.0.1:8000/ask/', contactFormData)
-    //     .then((response)=>{
-    //     setContactData({
-    //       'name':'',
-    //       'email': '',
-    //       'phone': '',
-    //       'describe': '',
-    //       'status': 'success',
-    //     });
-    //   })
-    // }catch (error){
-    //   console.log(error);
-    //   // setContactData({'status': 'error'})
-    // }
+    try {
+      axios.post('http://127.0.0.1:8000/ask/', contactFormData)
+        .then((response) => {
+          setContactData({
+            'name': '',
+            'email': '',
+            'phone': '',
+            'describe': '',
+            'status': 'success',
+          });
+        })
+    } catch (error) {
+      console.log(error);
+      setContactData({status: 'error'})
+    }
+  }
 
 
 return (
@@ -209,7 +128,7 @@ return (
                   <textarea
                     className="form-control"
                     id="message"
-                    type="text"
+                    datatype="text"
                     placeholder="Enter your message here..."
                     defaultValue={contactData.describe}
                     onChange={change}
@@ -243,7 +162,7 @@ return (
                     Error sending message!
                   </div>
                 </div>
-                {/* <!-- Submit Button--> */}
+                 {/*// <!-- Submit Button-->*/}
                 <div className="d-grid">
                   <button
                     className="btn btn-primary btn-lg"
---
+++ src/components/notes.js
@@ -0,0 +1,82 @@
+//     fetch(`http://127.0.0.1:8000/ask/`, {
+//       mode:"no-cors",
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         "Authorization": "Token b50f45d0c4e01d9a4ef2991bf1cdf85cf025f852"
+//       },
+//    body: JSON.stringify({
+//      // your expected POST request payload goes here
+//         title: "Dont look up",
+//         description: "comedy"
+//       })
+// })
+//   .then(resp => resp.json())
+//   .then(resp => console.log(resp)
+    // {
+    //   // enter you logic when the fetch is successful
+    //   setContactData({
+    //     'name': '',
+    //     'email': '',
+    //     'phone': '',
+    //     'describe': '',
+    //     'status': 'success',
+    //   }
+    //   ).catch(error=>console.log(error))
+    // }
+//   fetch(`http://127.0.0.1:8000/ask/`, {
+//       method: 'POST',
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         'Content-Type': 'application/json',
+//         "Authorization": "Token 757ab0fafa75f4c43646eedc5c8a83fc6b358592"
+//       },
+//    body: JSON.stringify({
+//      // your expected POST request payload goes here
+//         name: "Shaheer",
+//         email: "shaheerr7@gmail.com",
+//         phone:"3127950074",
+//         describe: "Help me"
+//       })
+// })
+//   .then(resp => resp.json())
+//   .then(resp => console.log(resp)
+//     // {
+//     //   // enter you logic when the fetch is successful
+//     //   setContactData({
+//     //     'name': '',
+//     //     'email': '',
+//     //     'phone': '',
+//     //     'describe': '',
+//     //     'status': 'success',
+//     //   }
+//       ).catch(error=>console.log(error))
+//     }
+//
+  
+    // const axiosPostCall = async () => {
+    //   try {
+    //     const { data } = await axios.post('http://127.0.0.1:8000/ask/', {
+    //       name: contactData.name,
+    //       email: contactData.email,
+    //       phone:contactData.phone,
+    //       describe: contactData.describe
+    //     }).then((response)=>{setContactData({
+    //         'name':'',
+    //         'email': '',
+    //         'phone': '',
+    //         'describe': '',
+    //         'status': 'success',
+    //       });
+    //     })
+    //  // enter you logic when the fetch is successful
+    //     console.log("success")
+    //
+    //   } catch (error) {
+    // // enter your logic for when there is an error (ex. error toast)
+    //     console.log(`error: `, error)
+    //     setContactData({status: 'error'})
+    //   }
+    // }
+    // axiosPostCall()
+  
