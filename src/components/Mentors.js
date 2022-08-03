01cc10db9f43c08fc05c495017ba5851e689245d
ShaheerRehman <shaheerr7@gmail.com>
2022-08-03 10:36:49 +0500

    added profile pics

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 
+// const imageUrl = "http://127.0.0.1:8000/media/default.jpg";
+
 function Mentors() {
   const [Mentors, setMentors] = useState([]);
+  // const [Img, setImg] = useState();
   //Write fetch logic here.
   const getMentors = async () => {
     const res = await fetch("http://127.0.0.1:8000/mentors/");
@@ -9,9 +12,17 @@ function Mentors() {
     //console.log(json);
     setMentors(json);
   };
+  // const fetchImage = async () => {
+  //   const res = await fetch(imageUrl);
+  //   const imageBlob = await res.blob();
+  //   const imageObjectURL = URL.createObjectURL(imageBlob);
+  //   setImg(imageObjectURL);
+  // };
 
   useEffect(() => {
     getMentors();
+    // fetchImage();
+
   }, []);
 
   return (
@@ -24,11 +35,14 @@ function Mentors() {
           {Mentors.map((mentor) => {
             return (
               <div className="col-9 col-md-6 p-5">
-                {/* <img
-                  src=""
-                  className="rounded mx-auto"
+                {/*<img src={Img} alt="icons" />*/}
+                <div className='text-center'>
+                <img
+                  src={mentor.dp}
+                  className="img-fluid rounded img-thumbnail"
                   alt={`${mentor.name} pic`}
-                /> */}
+                />
+                  </div>
                 <h2 className="text-center">{mentor.name}</h2>
                 <p className="text-center lead mb-0">{mentor.speciality}</p>
                 <p className="text-center">{mentor.description}</p>
